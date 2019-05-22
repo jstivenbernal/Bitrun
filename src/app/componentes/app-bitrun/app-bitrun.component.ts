49cf5d98fc4f2110b6fc4ee24497b12ba9aa4113
Jeisson Bernal <jeisson.bernal@tecnosoftit.com>
2019-05-22 13:42:09 -0500

    primer
    cambio

---
+++ src/app/componentes/app-bitrun/app-bitrun.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-bitrun',
+  templateUrl: './app-bitrun.component.html',
+  styleUrls: ['./app-bitrun.component.css']
+  /* styles: [] */
+})
+export class AppBitrunComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
